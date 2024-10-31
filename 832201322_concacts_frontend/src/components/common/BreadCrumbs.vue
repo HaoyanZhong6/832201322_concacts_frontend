359e9a346a4b31b8364efc0e7e71362cf65887e1
uleong <uleong>
2024-10-31 17:00:41 +0800

    commit

---
+++ 832201322_concacts_frontend/src/components/common/BreadCrumbs.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-breadcrumb class="breadcrumbs" separator="/">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <!-- <el-breadcrumb-item>联系人管理</el-breadcrumb-item>
+    <el-breadcrumb-item :to="{ path: '/userList' }">联系人列表</el-breadcrumb-item> -->
+  </el-breadcrumb>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.breadcrumbs {
+  padding: 20px;
+  background: #ffffff;
+}
+</style>
